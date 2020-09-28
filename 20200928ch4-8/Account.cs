022fa436cfd593d84adc17946fbb926316685614
NoZ-design <68496572+NoZ-design@users.noreply.github.com>
2020-09-28 16:37:25 +0800

    20200928ch4-8

---
+++ 20200928ch4-8/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _20200928ch4_8
+{
+    class Account
+    {
+        public string name { get; set; }
+
+        public Account(string accountName)
+        {
+            name = accountName;
+        }
+    }
+}
---
+++ 20200928ch4-8/AccountTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _20200928ch4_8
+{
+    class AccountTest
+    {
+        static void Main(string[] args)
+        {
+            Account account1 = new Account("name1");
+            Account account2 = new Account("name2");
+
+            Console.WriteLine($"account1's name is:{account1.name}");
+            Console.WriteLine($"account2's name is:{account2.name}");
+            Console.ReadKey();
+        }
+    }
+}
